1f1a1123cd4e3421c7a63fb8a3027d3514f5df7c
AmitKarnam <amitkarnam01@gmail.com>
2022-04-04 14:53:17 +0530

    Added environment variables and configured their usage.

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express  = require('express');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+// The dotenv.config() function from the dotenv npm package will read the .env file, assign the variables to process.env, and 
+// return an object (named parsed) containing the content. it will also throw an error if it failed.
+dotenv.config();
 
 const api_routes = require('./routes/api');
-const general_routes = require('./routes/general');
+
 
 const app = express();
 
@@ -11,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended:false }));
 
 // Url to mongo db atlas
-const url = 'mongodb+srv://AmitKarnam:OyuJo9QRqNEnRGN6@cluster0.rb1uc.mongodb.net/NoteBookDatabase?retryWrites=true&w=majority';
+const url = process.env.Mongo_Url;
 
 //Connecting to Database
 mongoose.connect(url).then( () => {
@@ -21,7 +26,6 @@ mongoose.connect(url).then( () => {
 });
 
 app.use('/api/books',api_routes);
-app.use('',general_routes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
