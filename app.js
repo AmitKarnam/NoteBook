8eb453107c0706527ee4b4864a4ca79c79380e2e
AmitKarnam <amitkarnam01@gmail.com>
2022-04-28 16:33:34 +0530

    Added Register and Login route and program code.

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express  = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 // The dotenv.config() function from the dotenv npm package will read the .env file, assign the variables to process.env, and 
 // return an object (named parsed) containing the content. it will also throw an error if it failed.
 dotenv.config();
 
+//importing api routes
 const api_routes = require('./routes/api');
 
 
@@ -26,6 +29,80 @@ mongoose.connect(url).then( () => {
 });
 
 app.use('/api/books',api_routes);
+
+//importing user context
+const User = require("./models/users");
+
+//Register
+app.post("/register" ,async (req,res) => {
+    try{
+        const {firstName, lastName, email, password } = req.body;
+
+        if ( !(email && password && firstName && lastName)){
+            res.status(400).json({msg:"All input are required"});
+        }
+
+        const oldUser = await User.findOne({email});
+
+        if(oldUser){
+            return res.status(400).json({msg:"User Already Exist. Please Login"});
+        }
+
+        encryptedUserPasswd = await bcrypt.hash(password,10);
+
+        const user = await User.create({
+            first_name:firstName,
+            last_name:lastName,
+            email:email.toLowerCase(),
+            password:encryptedUserPasswd,
+        });
+
+        // Create jwt token
+        const token = jwt.sign(
+            {user_id:user._id,email},
+            process.env.TOKEN_KEY,
+            {
+                expiresIn : "5h",
+            }
+        );
+
+        user.token = token;
+
+        res.status(201).json(user);
+    }catch(err){
+        console.log(err);
+    }
+});
+
+//Login
+app.post("/login", async (req,res) => {
+
+        const {email,password} = req.body;
+
+        if(!(email && password)){
+            res.status(400).json({msg:"All input are required"})
+        }
+
+        const user = await User.findOne({email});
+
+        if(user && (await bcrypt.compare(password,user.password))){
+            const token = jwt.sign(
+                {user_id : user._id,email},
+                process.env.TOKEN_KEY,
+                {
+                    expiresIn:"5h",
+                }
+            );
+
+            user.token = token;
+
+            return res.status(200).json(user);
+        }
+
+        return res.status(400).json({msg:"Invalid Credentials"});
+
+    }
+)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
